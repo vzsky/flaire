7e107853f4f4d5ebe70ef4a81f7e44c4dd8ca174
Touch Sungkawichai <talay@10ninox.com>
2020-04-22 20:09:27 +0700

    mutate initial

--- api/src/user/Guard.ts
+++ api/src/user/Guard.ts
@@ -1,9 +1,9 @@
-import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common"
+import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common'
 
 @Injectable()
 export class AdminGuard implements CanActivate {
-	canActivate(context: ExecutionContext): boolean {
-		const req = context.switchToHttp().getRequest()
-		return req.user.role === 1
-	}
+    canActivate(context: ExecutionContext): boolean {
+        const req = context.switchToHttp().getRequest()
+        return req.user.role === 1
+    }
 }
--- api/src/user/user.model.ts
+++ api/src/user/user.model.ts
@@ -1,26 +1,26 @@
-import * as mongoose from "mongoose"
+import * as mongoose from 'mongoose'
 
 export interface User extends mongoose.Document {
-	name: string
-	password: string
-	email: string
-	role: number
-	tasks: object
+    name: string
+    password: string
+    email: string
+    role: number
+    tasks: object
 }
 
 export const UserSchema = new mongoose.Schema({
-	name: String,
-	password: String,
-	email: String,
-	role: Number,
-	tasks: Object,
+    name: String,
+    password: String,
+    email: String,
+    role: Number,
+    tasks: Object,
 })
 
 export class UserDoc {
-	_id: string
-	name: string
-	password: string
-	email: string
-	role: number
-	tasks: object
+    _id: string
+    name: string
+    password: string
+    email: string
+    role: number
+    tasks: object
 }
